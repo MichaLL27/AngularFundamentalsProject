8e7138226355a6a9e31e8e7a11074af70939eb8f
MichaLL27 <lazarashvili2704@gmail.com>
2022-11-27 00:36:19 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { EventAppComponent } from './event-app.component';
+import { EventListComponent } from './events/event-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+// import { EventDetailsComponent } from './events/event-detais/event-details.component';
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [
-    AppComponent
+    EventAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/event-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app',
+  template: `
+    <nav-bar></nav-bar>
+    <event-list></event-list>
+  `,
+})
+export class EventAppComponent {
+  title = 'ng-fundamentals';
+}
