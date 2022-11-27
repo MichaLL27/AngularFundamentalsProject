58006acfe08c3489bfbf25f7d46d7e0366f18e2b
MichaLL27 <lazarashvili2704@gmail.com>
2022-11-27 14:08:16 +0400

    create routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { RouterModule } from '@angular/router';
 import { EventAppComponent } from './event-app.component';
 import { EventListComponent } from './events/event-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventService } from './events/shared/events.service';
 import { NavBarComponent } from './nav/navbar.component';
-// import { EventDetailsComponent } from './events/event-detais/event-details.component';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   declarations: [
     EventAppComponent,
     EventListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent,
   ],
+  providers: [EventService, ToastrService],
   bootstrap: [EventAppComponent],
 })
 export class AppModule {}
