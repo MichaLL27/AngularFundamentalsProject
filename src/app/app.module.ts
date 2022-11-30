31c829028a1959746ab7da4241bbbd76cc17a2e3
MichaLL27 <lazarashvili2704@gmail.com>
2022-11-30 18:26:14 +0400

    create all import in one import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import {
+  EventListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+} from './events/index';
+
 import { EventAppComponent } from './event-app.component';
-import { EventListComponent } from './events/event-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventService } from './events/shared/events.service';
 import { NavBarComponent } from './nav/navbar.component';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './error/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
@@ -22,7 +25,7 @@ import { EventListResolver } from './events/events-list-resolver.service';
     EventListComponent,
     EventThumbnailComponent,
     EventDetailsComponent,
-    NavBarComponent,
+    NavBarComponent, 
     CreateEventComponent,
     Error404Component,
   ],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { EventListComponent } from './events/event-list.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
+import {
+  EventListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+} from './events/index';
 import { Error404Component } from './error/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
 
 export const appRoutes: Routes = [
   {
