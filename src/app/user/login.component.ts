81a3869e3d41eda326e300f5aebef475ea4bab69
MichaLL27 <lazarashvili2704@gmail.com>
2022-12-01 16:30:38 +0400

    create form router and login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
@@ -7,7 +9,13 @@ export class LoginComponent {
   userName: any;
   password: any;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
   login(formValues: any) {
-    console.log(formValues);
+    this.authService.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
