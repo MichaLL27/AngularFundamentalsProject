c11a0cbc3bba99a8e45a4a31cf3e6e846bee2847
MichaLL27 <lazarashvili2704@gmail.com>
2022-12-01 18:00:14 +0400

    fix login problem

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,20 @@ import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName: any;
   password: any;
+  mouseoverLogin: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
